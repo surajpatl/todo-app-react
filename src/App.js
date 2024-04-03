609ed800c6077dd2837a4d9cec80aa66c688cc2f
suraj <surajpatil6040@gmail.com>
2024-04-04 01:07:07 +0530

    complete with add task, remove task, edit task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import Task from "./Task";
 import TaskForm from "./TaskForm";
+import Task from "./Task";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -12,10 +12,8 @@ function App() {
   }, [tasks]);
 
   useEffect(() => {
-    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
-    if (storedTasks) {
-      setTasks(storedTasks);
-    }
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    setTasks(tasks || []);
   }, []);
 
   function addTask(name) {
@@ -24,10 +22,57 @@ function App() {
     });
   }
 
+  function removeTask(indexToRemove) {
+    setTasks((prev) => {
+      return prev.filter((taskObject, index) => index !== indexToRemove);
+    });
+  }
+
+  function updateTaskDone(taskIndex, newDone) {
+    setTasks((prev) => {
+      const newTasks = [...prev];
+      newTasks[taskIndex].done = newDone;
+      return newTasks;
+    });
+  }
+
+  const numberComplete = tasks.filter((t) => t.done).length;
+  const numberTotal = tasks.length;
+
+  function getMessage() {
+    const percentage = (numberComplete / numberTotal) * 100;
+    if (percentage === 0) {
+      return "Try to do at least one! 🙏";
+    }
+    if (percentage === 100) {
+      return "Nice job for today! 🏝";
+    }
+    return "Keep it going 💪🏻";
+  }
+
+  function renameTask(index, newName) {
+    setTasks((prev) => {
+      const newTasks = [...prev];
+      newTasks[index].name = newName;
+      return newTasks;
+    });
+  }
+
   return (
     <main>
+      <h1>
+        {numberComplete}/{numberTotal} Complete
+      </h1>
+      <h2>{getMessage()}</h2>
       <TaskForm onAdd={addTask} />
-      {tasks && tasks.map((task, index) => <Task key={index} {...task} />)}
+      {tasks.map((task, index) => (
+        <Task
+          {...task}
+          onRename={(newName) => renameTask(index, newName)}
+          onTrash={() => removeTask(index)}
+          onToggle={(done) => updateTaskDone(index, done)}
+        />
+      ))}
     </main>
   );
 }
